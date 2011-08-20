020810e476a2fc189b82f1556b1f1fece9ec773c
gwtdeveloper@gmail.com <gwtdeveloper@gmail.com@d07c7dae-389c-8cfd-3279-14696a96f80a>
2011-08-20 02:10:32 +0000

    Add some tests

---
+++ trunk/test/com/gdevelop/gwt/syncrpc/test/TypeCheckedObjectsTest.java
@@ -0,0 +1,109 @@
+package com.gdevelop.gwt.syncrpc.test;
+
+import com.gdevelop.gwt.syncrpc.SyncProxy;
+
+import com.google.gwt.user.client.rpc.EnumsTestService;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.TypeCheckedGenericClass;
+import com.google.gwt.user.client.rpc.TypeCheckedObjectsTestService;
+
+import com.google.gwt.user.client.rpc.TypeCheckedObjectsTestSetFactory;
+import com.google.gwt.user.client.rpc.TypeCheckedObjectsTestSetFactory.TypeCheckedFieldClass;
+import com.google.gwt.user.client.rpc.TypeCheckedObjectsTestSetFactory.TypeCheckedSuperClass;
+
+import com.google.gwt.user.client.rpc.TypeCheckedObjectsTestSetValidator;
+import com.google.gwt.user.client.rpc.TypeUncheckedGenericClass;
+
+import java.util.HashSet;
+
+import junit.framework.TestCase;
+
+public class TypeCheckedObjectsTest extends TestCase{
+  private static TypeCheckedObjectsTestService service = 
+    (TypeCheckedObjectsTestService)SyncProxy.newProxyInstance(
+        TypeCheckedObjectsTestService.class, RPCSyncTestSuite.BASE_URL, 
+        "enums");
+
+  public TypeCheckedObjectsTest() {
+  }
+  
+  public void testInvalidCheckedFieldSerializer() {
+    TypeCheckedFieldClass arg1 = 
+      TypeCheckedObjectsTestSetFactory.createInvalidCheckedFieldClass();
+    try{
+      service.echo(arg1);
+      fail("testInvalidCheckedFieldSerializer is expected to throw an assertion");
+    }catch(Exception caught){
+      assertTrue(caught instanceof SerializationException);
+    }
+  }
+  
+  public void testInvalidCheckedSerializer() {
+    TypeCheckedGenericClass arg = TypeCheckedObjectsTestSetFactory.createInvalidCheckedGenericClass();
+    try{
+      service.echo(arg);
+      fail("testInvalidCheckedSerializer is expected to throw an assertion");
+    }catch(Exception caught){
+      assertTrue(caught instanceof SerializationException);
+    }
+  }
+  
+  public void testInvalidCheckedSuperSerializer() {
+    TypeCheckedSuperClass arg = 
+      TypeCheckedObjectsTestSetFactory.createInvalidCheckedSuperClass();
+    try{
+      service.echo(arg);
+      fail("testInvalidCheckedSuperSerializer is expected to throw an assertion");
+    }catch(Exception caught){
+      assertTrue(caught instanceof SerializationException);
+    }
+  }
+  
+  public void testInvalidUncheckedSerializer() {
+    TypeUncheckedGenericClass arg = 
+      TypeCheckedObjectsTestSetFactory.createInvalidUncheckedGenericClass();
+    try{
+      service.echo(arg);
+      fail("testInvalidUncheckedSerializer is expected to throw an assertion");
+    }catch(Exception caught){
+      assertTrue(caught instanceof SerializationException);
+    }
+  }
+  
+  public void testTypeCheckedFieldSerializer() {
+    TypeCheckedFieldClass arg = 
+      TypeCheckedObjectsTestSetFactory.createTypeCheckedFieldClass();
+    
+    TypeCheckedFieldClass result = service.echo(arg);
+    assertNotNull(result);
+    assertTrue(TypeCheckedObjectsTestSetValidator.isValid(result));
+  }
+  
+  public void testTypeCheckedSerializer() {
+    TypeCheckedGenericClass arg = 
+      TypeCheckedObjectsTestSetFactory.createTypeCheckedGenericClass();
+    
+    TypeCheckedGenericClass result = service.echo(arg);
+    assertNotNull(result);
+    assertTrue(TypeCheckedObjectsTestSetValidator.isValid(result));
+  }
+  
+  public void testTypeCheckedSuperSerializer() {
+    TypeCheckedSuperClass arg = 
+      TypeCheckedObjectsTestSetFactory.createTypeCheckedSuperClass();
+    
+    TypeCheckedSuperClass result = service.echo(arg);
+    assertNotNull(result);
+    assertTrue(TypeCheckedObjectsTestSetValidator.isValid(result));
+  }
+  
+  public void testTypeUncheckedSerializer() {
+    TypeUncheckedGenericClass arg = 
+      TypeCheckedObjectsTestSetFactory.createTypeUncheckedGenericClass();
+    
+    TypeUncheckedGenericClass result = service.echo(arg);
+    assertNotNull(result);
+    assertTrue(TypeCheckedObjectsTestSetValidator.isValid(result));
+  }
+}
