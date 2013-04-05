718dff89464def30de7394a3e2cb33e275a1e133
p.prith@blueesoteric.com <p.prith@blueesoteric.com@d07c7dae-389c-8cfd-3279-14696a96f80a>
2013-04-05 20:37:30 +0000

    next commit with new structure, for the Android Library
    See the Android wiki for details on use. Current version is dependent on the gwt-servlet.jar. An update in short order will attempt to remove this dependency, as described in the wiki.

---
+++ branches/AndroidLibrary/SPALibrary/src/com/gdevelop/gwt/syncrpc/android/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>
+ * Please refer to the Google Code Wiki for more details on coding and utilizing the services available for this
+ * library.
+ * </p>
+ * @since 0.4
+ */
+package com.gdevelop.gwt.syncrpc.android;
