d424a1b4f9d1bed55bcbf0f1e341f180ca145bc3
Preethum <p.prith@blueesoteric.com>
2015-05-04 01:25:58 -0400

    server launch successfully received and tested unit tests from java and Android device clients

--- gwt-syncproxy/test/com/google/gwt/user/client/rpc/RpcTestBase.java
+++ gwt-syncproxy/test/com/google/gwt/user/client/rpc/RpcTestBase.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -76,7 +76,7 @@ public class RpcTestBase extends TestCase {
 	}
 
 	public String getBaseURL() {
-		return "http://127.0.0.1:8888/";
+		return "http://127.0.0.1:8082/";
 	}
 
 	public String getModuleName() {
@@ -118,8 +118,7 @@ public class RpcTestBase extends TestCase {
 		super.tearDown();
 	}
 
-	protected void waitForRpcFinish(String message, int seconds)
-			throws InterruptedException {
+	protected void waitForRpcFinish(String message, int seconds) throws InterruptedException {
 		if (this.signal != null) {
 			this.logger.info("Waiting on Signal: " + this.signal);
 			assertTrue(message, this.signal.await(seconds, TimeUnit.SECONDS));
